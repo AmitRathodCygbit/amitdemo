98ba8eb9973f57f88ffba5acc4df4f666bfd9f50
AmitRathodCygbit <amit.rathod@cygbit.com>
2022-11-29 16:48:30 +0530

    first file change and app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import First from './component/First';
 function App() {
   return (
     <div className="App">
       <h1>AMIT DATA</h1>
+      <First/>
     </div>
   );
 }
---
+++ src/component/First.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function First() {
+  return (
+    <div>
+<h2>First File </h2>
+    </div>
+  )
+}
+
+export default First
